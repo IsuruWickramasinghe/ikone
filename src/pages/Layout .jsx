e82d723edf42ab99001f6829823c0d9ec72ead32
isuruwebdev <mohanisuruw@gmail.com>
2023-11-06 18:48:37 +0530

    countdown timer

--- src/pages/Layout .jsx
+++ src/pages/Layout .jsx
@@ -2,8 +2,23 @@
 import NavBar from '../components/NavBar/NavBar';
 import Footer from '../components/Footer/Footer';
 import Copyringt from '../components/CopyRights/Copyringt';
+import { useState } from 'react';
+import MainCountDown from '../Temp/MainCountDown';
+
+
 
 function Layout({ children }) {
+
+  const [timerEnd,setTimerEnd] = useState(false)
+
+  if(!timerEnd){
+    return(
+      <div className="countDonwTimer">
+        <MainCountDown setTimerEnd={setTimerEnd} />
+      </div>
+    )
+  }
+
   return (
       <div className="layout">
         <nav className="nav-bar">
