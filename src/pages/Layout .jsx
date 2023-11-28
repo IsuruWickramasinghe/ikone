46761dfc093c2cc6486ec989df036597ca81ed3a
isuruW <92825254+isuruwebdev@users.noreply.github.com>
2023-11-28 18:39:59 +0530

    IKONE IS UNDER MAINTENANCE

--- src/pages/Layout .jsx
+++ src/pages/Layout .jsx
@@ -4,20 +4,29 @@ import Footer from '../components/Footer/Footer';
 import Copyringt from '../components/CopyRights/Copyringt';
 
 function Layout({ children }) {
+
   return (
-      <div className="layout">
-        <nav className="nav-bar">
-          <NavBar />
-        </nav>
-        <main className="main">
-          {children}
-        </main>
-        <footer className="footer">
-          <Footer />
-        </footer>
-        <Copyringt />
-      </div>
-  );
+    <div className="maintain">
+      <div><h1>IKONE IS UNDER MAINTENANCE...</h1></div>
+      <img src="./clip.gif" alt="bv"/>
+    </div>
+  )
+
+
+  // return (
+  //     <div className="layout">
+  //       <nav className="nav-bar">
+  //         <NavBar />
+  //       </nav>
+  //       <main className="main">
+  //         {children}
+  //       </main>
+  //       <footer className="footer">
+  //         <Footer />
+  //       </footer>
+  //       <Copyringt />
+  //     </div>
+  // );
 }
 
 export default Layout;
