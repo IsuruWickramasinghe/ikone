c4af9182a37d11d872604c20cd9cbfc1713ab09b
isuruW <92825254+isuruwebdev@users.noreply.github.com>
2023-12-12 23:12:45 +0530

    site temp live

--- src/pages/Layout .jsx
+++ src/pages/Layout .jsx
@@ -5,28 +5,28 @@ import Copyringt from '../components/CopyRights/Copyringt';
 
 function Layout({ children }) {
 
-  return (
-    <div className="maintain">
-      <div><h1>IKONE IS UNDER MAINTENANCE...</h1></div>
-      <img src="./clip.gif" alt="bv"/>
-    </div>
-  )
-
-
   // return (
-  //     <div className="layout">
-  //       <nav className="nav-bar">
-  //         <NavBar />
-  //       </nav>
-  //       <main className="main">
-  //         {children}
-  //       </main>
-  //       <footer className="footer">
-  //         <Footer />
-  //       </footer>
-  //       <Copyringt />
-  //     </div>
-  // );
+  //   <div className="maintain">
+  //     <div><h1>IKONE IS UNDER MAINTENANCE...</h1></div>
+  //     <img src="./clip.gif" alt="bv"/>
+  //   </div>
+  // )
+
+
+  return (
+      <div className="layout">
+        <nav className="nav-bar">
+          <NavBar />
+        </nav>
+        <main className="main">
+          {children}
+        </main>
+        <footer className="footer">
+          <Footer />
+        </footer>
+        <Copyringt />
+      </div>
+  );
 }
 
 export default Layout;
