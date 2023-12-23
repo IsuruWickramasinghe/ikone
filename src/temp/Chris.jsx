743efc4436b8710cb218b98bd95a74dbdf7e2bad
isuruW <92825254+isuruwebdev@users.noreply.github.com>
2023-12-23 09:48:25 +0530

    new banner

---
+++ src/temp/Chris.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './temp.css'
+function Chris() {
+  return (
+    <div className='Chris'>
+      <img src="./temp.gif" alt="banner" />
+    </div>
+  )
+}
+
+export default Chris
